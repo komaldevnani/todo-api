60dea0cfc6d88e7700d46ea5bada144907b4cdd8
komal devnani <komaldevnani00@gmail.com>
2020-06-12 01:12:10 +0530

    authentication with jwt

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -11,4 +11,5 @@ module ExceptionHandler
       json_response({ message: e.message }, :unprocessable_entity)
     end
   end
+
 end
--- spec/requests/items_request_spec.rb
+++ spec/requests/items_request_spec.rb
@@ -3,14 +3,16 @@ require 'faker'
 
 RSpec.describe "Items API" do
   # Initialize the test data
+  let(:user) { create(:user) }
   let!(:todo) { create(:todo) }
   let!(:items) { create_list(:item, 20, todo_id: todo.id) }
   let(:todo_id) { todo.id }
   let(:id) { items.first.id }
+  let(:headers) { valid_headers }
 
   # Test suite for GET /todos/:todo_id/items
   describe 'GET /todos/:todo_id/items' do
-    before { get "/todos/#{todo_id}/items" }
+    before { get "/todos/#{todo_id}/items",params: {}, headers: headers }
 
     context 'when todo exists' do
       it 'returns status code 200' do
@@ -35,7 +37,7 @@ RSpec.describe "Items API" do
 
   # Test suite for GET /todos/:todo_id/items/:id
   describe 'GET /todos/:todo_id/items/:id' do
-    before { get "/todos/#{todo_id}/items/#{id}" }
+    before { get "/todos/#{todo_id}/items/#{id}", params: {}, headers: headers }
     context 'when todo item exists' do
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
@@ -59,17 +61,17 @@ RSpec.describe "Items API" do
   end
   # Test suite for PUT /todos/:todo_id/items
   describe 'POST /todos/:todo_id/items' do
-    let(:valid_attributes) { { name: 'Visit Narnia', done: false } }
+    let(:valid_attributes) { { name: 'Visit Narnia', done: false }.to_json }
 
     context 'when request attributes are valid' do
-      before { post "/todos/#{todo_id}/items", params: valid_attributes }
+      before { post "/todos/#{todo_id}/items", params: valid_attributes, headers: headers }
 
       it 'returns status code 201' do
         expect(response).to have_http_status(201)
       end
     end
     context 'when an invalid request' do
-      before { post "/todos/#{todo_id}/items", params: {} }
+      before { post "/todos/#{todo_id}/items", params: {}, headers: headers }
 
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
@@ -82,9 +84,9 @@ RSpec.describe "Items API" do
   end
   # Test suite for PUT /todos/:todo_id/items/:id
   describe 'PUT /todos/:todo_id/items/:id' do
-    let(:valid_attributes) { { name: 'Mozart' } }
+    let(:valid_attributes) { { name: 'Mozart' }.to_json }
 
-    before { put "/todos/#{todo_id}/items/#{id}", params: valid_attributes }
+    before { put "/todos/#{todo_id}/items/#{id}", params: valid_attributes, headers: headers }
 
     context 'when item exists' do
       it 'returns status code 204' do
@@ -109,7 +111,7 @@ RSpec.describe "Items API" do
   end
   # Test suite for DELETE /todos/:id
   describe 'DELETE /todos/:id' do
-    before { delete "/todos/#{todo_id}/items/#{id}" }
+    before { delete "/todos/#{todo_id}/items/#{id}",params: {}, headers: headers }
 
     it 'returns status code 204' do
       expect(response).to have_http_status(204)
